6cb1e94d5c8b206fc93f18f66fcd540472b92e82
mazhenkai <523395363@qq.com>
2018-11-03 13:50:25 +0800

    数据库重新备份

--- django_web/dis_dashboard/load_list_data.py
+++ django_web/dis_dashboard/load_list_data.py
@@ -1,28 +1,205 @@
 import csv
 from dis_dashboard.models import *
 
-path = r'data_normal/AreaList.csv'
-with open(path) as f:
-    reader = csv.reader(f)
-    next(reader, None)
-    for row in reader:
-        row = row[0].split('\t')
-        print(row)
-        _, created = AreaList.objects.get_or_create(
-            area_name=row[0],
-            area_name_reg=row[1],
-            area_name_short=row[2],
-        )
 
+def run():
+    # path = r'need_data/agg_code.csv'
+    # n_row = len(list(csv.reader(open(path))))
+    # split = int(n_row / n_row)
+    # split_list = range(0, n_row, split)
+    # print(split_list)
+    # count = 0
+    # with open(path) as csvfile:
+    #     reader = csv.DictReader(csvfile)
+    #     for row in reader:
+    #         p = AggCode(
+    #             agg_id=row["agg_id"],
+    #             ag_g=row["ag_g"]
+    #         )
+    #         p.save()
+    #         # print(row)
+    #         if count in split_list:
+    #             print(count / n_row)
+    #             print(row)
+    #         count = count + 1
+    #
+    # path = r'need_data/agg_tb.csv'
+    # n_row = len(list(csv.reader(open(path))))
+    # split = int(n_row / 100)
+    # split_list = range(0, n_row, split)
+    # print(split_list)
+    # count = 0
+    # with open(path) as csvfile:
+    #     reader = csv.DictReader(csvfile)
+    #     for row in reader:
+    #         p = AggTb(
+    #             year=row["year"],
+    #             month=row["month"],
+    #             reg_id=row["reg_id"],
+    #             dis_id=row["dis_id"],
+    #             agg_id=row["agg_id"],
+    #             gd_id=row["gd_id"],
+    #             case_num=row["case_num"],
+    #             dea_num=row["dea_num"],
+    #             inci_rate=row["inci_rate"],
+    #             mort_rate=row["mort_rate"]
+    #         )
+    #         p.save()
+    #         # print(row)
+    #         if count in split_list:
+    #             print(count / n_row)
+    #             print(row)
+    #         count = count + 1
+    #
+    #     path = r'need_data/dis_code.csv'
+    #     n_row = len(list(csv.reader(open(path))))
+    #     split = int(n_row / n_row)
+    #     split_list = range(0, n_row, split)
+    #     print(split_list)
+    #     count = 0
+    #     with open(path) as csvfile:
+    #         reader = csv.DictReader(csvfile)
+    #         for row in reader:
+    #             p = DisCode(
+    #                 dis_id=row["dis_id"],
+    #                 dis_name=row["dis_name"]
+    #             )
+    #             p.save()
+    #             # print(row)
+    #             if count in split_list:
+    #                 print(count / n_row)
+    #                 print(row)
+    #             count = count + 1
+    #
+    #     path = r'need_data/gd_code.csv'
+    #     n_row = len(list(csv.reader(open(path))))
+    #     split = int(n_row / n_row)
+    #     split_list = range(0, n_row, split)
+    #     print(split_list)
+    #     count = 0
+    #     with open(path) as csvfile:
+    #         reader = csv.DictReader(csvfile)
+    #         for row in reader:
+    #             p = GdCode(
+    #                 gd_id=row["gd_id"],
+    #                 gender=row["gender"]
+    #             )
+    #             p.save()
+    #             # print(row)
+    #             if count in split_list:
+    #                 print(count / n_row)
+    #                 print(row)
+    #             count = count + 1
+    #
+    #     path = r'need_data/month_code.csv'
+    #     n_row = len(list(csv.reader(open(path))))
+    #     split = int(n_row / n_row)
+    #     split_list = range(0, n_row, split)
+    #     print(split_list)
+    #     count = 0
+    #     with open(path) as csvfile:
+    #         reader = csv.DictReader(csvfile)
+    #         for row in reader:
+    #             p = MonthCode(
+    #                 month=row["month"],
+    #                 month_cn=row["month_cn"]
+    #             )
+    #             p.save()
+    #             # print(row)
+    #             if count in split_list:
+    #                 print(count / n_row)
+    #                 print(row)
+    #             count = count + 1
+    #
+    #     path = r'need_data/pro_code.csv'
+    #     n_row = len(list(csv.reader(open(path))))
+    #     split = int(n_row / n_row)
+    #     split_list = range(0, n_row, split)
+    #     print(split_list)
+    #     count = 0
+    #     with open(path) as csvfile:
+    #         reader = csv.DictReader(csvfile)
+    #         for row in reader:
+    #             p = ProCode(
+    #                 pro_name=row["pro_name"],
+    #                 pro_id=row["pro_id"]
+    #             )
+    #             p.save()
+    #             # print(row)
+    #             if count in split_list:
+    #                 print(count / n_row)
+    #                 print(row)
+    #             count = count + 1
+    #
+    #     path = r'need_data/pro_tb.csv'
+    #     n_row = len(list(csv.reader(open(path))))
+    #     split = int(n_row / 100)
+    #     split_list = range(0, n_row, split)
+    #     print(split_list)
+    #     count = 0
+    #     with open(path) as csvfile:
+    #         reader = csv.DictReader(csvfile)
+    #         for row in reader:
+    #             p = ProTb(
+    #                 year=row["year"],
+    #                 month=row["month"],
+    #                 reg_id=row["reg_id"],
+    #                 dis_name=row["dis_name"],
+    #                 pro_name=row["pro_name"],
+    #                 case_num=row["case_num"],
+    #                 dea_num=row["dea_num"],
+    #                 inci_rate=row["inci_rate"],
+    #                 mort_rate=row["mort_rate"]
+    #             )
+    #             p.save()
+    #             # print(row)
+    #             if count in split_list:
+    #                 print(count / n_row)
+    #                 print(row)
+    #             count = count + 1
 
-path = r'data_normal/DisList.csv'
-with open(path) as f:
-    reader = csv.reader(f)
-    next(reader, None)
-    for row in reader:
-        row = row[0].split('\t')
-        print(row)
-        _, created = DisList.objects.get_or_create(
-            dis_name=row[0],
-        )
+        path = r'need_data/reg_code.csv'
+        n_row = len(list(csv.reader(open(path))))
+        split = int(n_row / n_row)
+        split_list = range(0, n_row, split)
+        print(split_list)
+        count = 0
+        with open(path) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                p = RegCode(
+                    reg_id=row["reg_id"],
+                    reg_name=row["reg_name"]
+                )
+                p.save()
+                # print(row)
+                if count in split_list:
+                    print(count / n_row)
+                    print(row)
+                count = count + 1
 
+        # path = r'need_data/reg_tb.csv'
+        # n_row = len(list(csv.reader(open(path))))
+        # split = int(n_row / 100)
+        # split_list = range(0, n_row, split)
+        # print(split_list)
+        # count = 0
+        # with open(path) as csvfile:
+        #     reader = csv.DictReader(csvfile)
+        #     for row in reader:
+        #         p = RegTb(
+        #             year=row["year"],
+        #             month=row["month"],
+        #             reg_id=row["reg_id"],
+        #             disease=row["disease"],
+        #             case_num=row["case_num"],
+        #             dea_num=row["dea_num"],
+        #             inci_rate=row["inci_rate"],
+        #             mort_rate=row["mort_rate"]
+        #         )
+        #         p.save()
+        #         # print(row)
+        #         if count in split_list:
+        #             print(count / n_row)
+        #             print(row)
+        #         count = count + 1
--- django_web/dis_dashboard/models.py
+++ django_web/dis_dashboard/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 
+class AggCode(models.Model):
+    agg_id = models.CharField(max_length=50, unique=True)
+    ag_g = models.CharField(max_length=50)
+
+
 class AggTb(models.Model):
     year = models.CharField(max_length=50)
     month = models.CharField(max_length=50)
@@ -12,6 +17,16 @@ class AggTb(models.Model):
     dea_num = models.IntegerField()
     inci_rate = models.DecimalField(max_digits=10, decimal_places=4)
     mort_rate = models.DecimalField(max_digits=10, decimal_places=4)
+
+
+class DisCode(models.Model):
+    dis_id = models.CharField(max_length=50, unique=True)
+    dis_name = models.CharField(max_length=50)
+
+
+class GdCode(models.Model):
+    gd_id = models.CharField(max_length=50, unique=True)
+    gender = models.CharField(max_length=50)
 
 
 class ProTb(models.Model):
@@ -26,37 +41,8 @@ class ProTb(models.Model):
     mort_rate = models.DecimalField(max_digits=10, decimal_places=4)
 
 
-
-class RegTb(models.Model):
-    year = models.CharField(max_length=50)
-    month = models.CharField(max_length=50)
-    reg_id = models.CharField(max_length=50)
-    disease = models.CharField(max_length=50)
-    case_num = models.IntegerField()
-    dea_num = models.IntegerField()
-    inci_rate = models.DecimalField(max_digits=10, decimal_places=4)
-    mort_rate = models.DecimalField(max_digits=10, decimal_places=4)
-
-
-
-
-class AggCode(models.Model):
-    agg_id = models.CharField(max_length=50)
-    ag_g = models.CharField(max_length=50)
-
-
-class DisCode(models.Model):
-    dis_id = models.CharField(max_length=50)
-    dis_name = models.CharField(max_length=50)
-
-
-class GdCode(models.Model):
-    gd_id = models.CharField(max_length=50)
-    gender = models.CharField(max_length=50)
-
-
 class MonthCode(models.Model):
-    month = models.CharField(max_length=50)
+    month = models.CharField(max_length=50, unique=True)
     month_cn = models.CharField(max_length=50)
 
 
@@ -69,3 +55,13 @@ class RegCode(models.Model):
     reg_id = models.CharField(max_length=50)
     reg_name = models.CharField(max_length=50)
 
+
+class RegTb(models.Model):
+    year = models.CharField(max_length=50)
+    month = models.CharField(max_length=50)
+    reg_id = models.CharField(max_length=50)
+    disease = models.CharField(max_length=50)
+    case_num = models.IntegerField()
+    dea_num = models.IntegerField()
+    inci_rate = models.DecimalField(max_digits=10, decimal_places=4)
+    mort_rate = models.DecimalField(max_digits=10, decimal_places=4)
