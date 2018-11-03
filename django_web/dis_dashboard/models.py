8bb774d06f5a99637d1d3231fed09fcc869e1f1a
mazhenkai <523395363@qq.com>
2018-11-03 11:50:59 +0800

    Revert "1"
    This reverts commit 95d3bd8cfdbec654aee05e3576ba8787222bebf4.

--- django_web/dis_dashboard/models.py
+++ django_web/dis_dashboard/models.py
@@ -3,12 +3,11 @@ from django.db import models
 
 class AggTb(models.Model):
     year = models.CharField(max_length=50)
-    # month = models.CharField(max_length=50)
-    month = models.ForeignKey('MonthCode', to_field="month", on_delete="SET_NULL")
-    reg_id = models.ForeignKey('RegCode', to_field="reg_id", on_delete="SET_NULL")
-    dis_id = models.ForeignKey('DisCode', to_field="dis_id", on_delete="SET_NULL")
-    agg_id = models.ForeignKey('AggCode', to_field="agg_id", on_delete="SET_NULL")
-    gd_id = models.ForeignKey('GdCode', to_field="gd_id", on_delete="SET_NULL")
+    month = models.CharField(max_length=50)
+    reg_id = models.CharField(max_length=50)
+    dis_id = models.CharField(max_length=50)
+    agg_id = models.CharField(max_length=50)
+    gd_id = models.CharField(max_length=50)
     case_num = models.IntegerField()
     dea_num = models.IntegerField()
     inci_rate = models.DecimalField(max_digits=10, decimal_places=4)
@@ -27,6 +26,7 @@ class ProTb(models.Model):
     mort_rate = models.DecimalField(max_digits=10, decimal_places=4)
 
 
+
 class RegTb(models.Model):
     year = models.CharField(max_length=50)
     month = models.CharField(max_length=50)
@@ -38,32 +38,34 @@ class RegTb(models.Model):
     mort_rate = models.DecimalField(max_digits=10, decimal_places=4)
 
 
+
+
 class AggCode(models.Model):
-    agg_id = models.CharField(max_length=50, unique=True)
+    agg_id = models.CharField(max_length=50)
     ag_g = models.CharField(max_length=50)
 
 
 class DisCode(models.Model):
-    dis_id = models.CharField(max_length=50, unique=True)
+    dis_id = models.CharField(max_length=50)
     dis_name = models.CharField(max_length=50)
 
 
 class GdCode(models.Model):
-    gd_id = models.CharField(max_length=50, unique=True)
+    gd_id = models.CharField(max_length=50)
     gender = models.CharField(max_length=50)
 
 
 class MonthCode(models.Model):
-    month = models.CharField(max_length=50, unique=True)
+    month = models.CharField(max_length=50)
     month_cn = models.CharField(max_length=50)
 
 
 class ProCode(models.Model):
-    pro_name = models.CharField(max_length=50, unique=True)
+    pro_name = models.CharField(max_length=50)
     pro_id = models.CharField(max_length=50)
 
 
 class RegCode(models.Model):
-    reg_id = models.CharField(max_length=50, unique=True)
+    reg_id = models.CharField(max_length=50)
     reg_name = models.CharField(max_length=50)
 
