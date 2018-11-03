3ea1dbf1d150297303979f62748126b85e4a9220
mazhenkai <523395363@qq.com>
2018-11-03 12:04:52 +0800

    1

--- django_web/dis_dashboard/models.py
+++ django_web/dis_dashboard/models.py
@@ -41,31 +41,31 @@ class RegTb(models.Model):
 
 
 class AggCode(models.Model):
-    agg_id = models.CharField(max_length=50)
+    agg_id = models.CharField(max_length=50, unique=True)
     ag_g = models.CharField(max_length=50)
 
 
 class DisCode(models.Model):
-    dis_id = models.CharField(max_length=50)
+    dis_id = models.CharField(max_length=50, unique=True)
     dis_name = models.CharField(max_length=50)
 
 
 class GdCode(models.Model):
-    gd_id = models.CharField(max_length=50)
+    gd_id = models.CharField(max_length=50, unique=True)
     gender = models.CharField(max_length=50)
 
 
 class MonthCode(models.Model):
-    month = models.CharField(max_length=50)
+    month = models.CharField(max_length=50, unique=True)
     month_cn = models.CharField(max_length=50)
 
 
 class ProCode(models.Model):
-    pro_name = models.CharField(max_length=50)
+    pro_name = models.CharField(max_length=50, unique=True)
     pro_id = models.CharField(max_length=50)
 
 
 class RegCode(models.Model):
-    reg_id = models.CharField(max_length=50)
+    reg_id = models.CharField(max_length=50, unique=True)
     reg_name = models.CharField(max_length=50)
 
